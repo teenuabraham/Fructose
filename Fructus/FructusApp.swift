68460b300667ded5bbe47495b9b86faf752a6ab8
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-03-29 13:12:13 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Teenu Abraham on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
